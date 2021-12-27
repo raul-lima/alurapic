84f68a7c4bbf15e646b31af892de99be0e774888
Raul Lima <raultorres.lima@gmail.com>
2021-12-27 17:26:06 -0300

    feat: router implementation

---
+++ src/components/cadastro/Cadastro.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2 class="centralizado">Cadastro</h2>
+</template>
+
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {},
+});
+</script>
+
+<style lang="sss" scoped>
+.centralizado {
+  text-align: center;
+}
+</style>
